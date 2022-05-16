3084095e5dcd2dd048aedcc985e3a6647e3efbd7
Mayo.nakano <pharm.yoma1983@gmail.com>
2022-05-16 01:00:14 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
